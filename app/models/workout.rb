1e2bf64f269872bf3503cf6ce2c1fadfe5bdb913
Sean Hanlon <shanlon800@gmail.com>
2018-02-05 15:01:14 -0500

    updated date to workout_date in model and tests

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -4,5 +4,5 @@ class Workout < ApplicationRecord
   validates :run_type, presence: true
   validates :distance, presence: true, numericality: { :greater_than_or_equal_to => 0}
   validates :time, presence: true
-  validates :date, presence: true
+  validates :workout_date, presence: true
 end
