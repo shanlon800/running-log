62ad0b6f06cb8a8084d68b944eb900f77b11b861
Sean Hanlon <shanlon800@gmail.com>
2018-02-05 11:54:05 -0500

    added validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :memberships
   has_many :teams,
     through: :memberships
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
