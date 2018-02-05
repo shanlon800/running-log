6cdeb73868322dfb63a14afc65b0a4ae823ba6e3
Sean Hanlon <shanlon800@gmail.com>
2018-02-05 11:48:16 -0500

    Adding users to db and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :workouts
+  has_many :memberships
+  has_many :teams,
+    through: :memberships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_valid(:first_name).when("John") }
+  it { should have_valid(:last_name).when("Smith") }
+
+  it { should_not have_valid(:first_name).when(nil,"") }
+  it { should_not have_valid(:last_name).when(nil,"") }
+
+  it {should have_valid(:email).when("jsmith@runlog.com")}
+  it {should have_valid(:password).when("password123")}
+
+  it { should_not have_valid(:email).when(nil,"") }
+  it { should_not have_valid(:password).when(nil,"") }
+
+
 end
