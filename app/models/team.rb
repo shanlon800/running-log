0f446f2a278563ce5b98c1b1427d87e9e12d5345
Sean Hanlon <shanlon800@gmail.com>
2018-02-05 16:49:09 -0500

    code climate fix

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,7 @@ class Team < ApplicationRecord
   has_many :memberships
   has_many :users,
     through: :memberships
-  belongs_to :coach, class_name: "User"
+  belongs_to :coach, class_name: 'User'
 
   validates :team_name, presence: true
 end
