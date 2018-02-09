32633ae7291391078e179f50ad966a5f01d9df29
Sean Hanlon <shanlon800@gmail.com>
2018-02-09 17:15:07 -0500

    setup carrierwave, started AWS s3, deleted .env

--- Gemfile
+++ Gemfile
@@ -28,7 +28,8 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
+gem "carrierwave"
+gem "fog-aws"
 group :development, :test do
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206182206) do
+ActiveRecord::Schema.define(version: 20180209202337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 20180206182206) do
     t.string "team_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "coach_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,7 +45,7 @@ ActiveRecord::Schema.define(version: 20180206182206) do
     t.datetime "updated_at", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
-    t.string "picture"
+    t.string "profile_photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
