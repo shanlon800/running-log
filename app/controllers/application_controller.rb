6c5edffcdf1bf3f05dee5d5bc2f740bd8259df6a
Sean Hanlon <shanlon800@gmail.com>
2018-02-10 10:15:44 -0500

    changed formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :remember_me, :profile_photo) }
+  devise_parameter_sanitizer.permit(:sign_up) {
+     |u| u.permit(
+      :first_name, 
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation,
+      :remember_me,
+      :profile_photo
+    )}
   # devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password,
   #   :password_confirmation, :current_password, :avatar, :avatar_cache, :remove_avatar) }
   end
