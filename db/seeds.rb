3416a65577b11a2e49910d07a320932e2804e1c9
Sean Hanlon <shanlon800@gmail.com>
2018-02-12 11:48:49 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,13 +21,13 @@ workouts = [{user_id: 1, distance: 5, time: 35, notes: 'felt pretty good today',
   {user_id: 2, distance: 10, time: 75, notes: 'Had a really tough run today', workout_date: 'Feb 16 2018'},
   {user_id: 2, distance: 2, time: 17, notes: 'Recovery Day', workout_date: 'Feb 17 2018'},
   {user_id: 2, distance: 15, time: 17, notes: 'Long Run Day', workout_date: 'Feb 18 2018'},
-  {user_id: 3, distance: 5, time: 35, notes: 'felt pretty good today', workout_date: 'Feb 12 2018'},
-  {user_id: 3, distance: 3.5, time: 22, notes: 'felt really fast today', workout_date: 'Feb 13 2018'},
-  {user_id: 3, distance: 2, time: 18, notes: 'felt really tired today', workout_date: 'Feb 14 2018'},
-  {user_id: 3, distance: 5, time: 37, notes: 'It is cold today', workout_date: 'Feb 15 2018'},
-  {user_id: 3, distance: 10, time: 75, notes: 'Had a really tough run today', workout_date: 'Feb 16 2018'},
-  {user_id: 3, distance: 2, time: 17, notes: 'Recovery Day', workout_date: 'Feb 17 2018'},
-  {user_id: 3, distance: 15, time: 17, notes: 'Long Run Day', workout_date: 'Feb 18 2018'}
+  {user_id: 8, distance: 5, time: 35, notes: 'felt pretty good today', workout_date: 'Feb 12 2018'},
+  {user_id: 8, distance: 3.5, time: 22, notes: 'felt really fast today', workout_date: 'Feb 13 2018'},
+  {user_id: 8, distance: 2, time: 18, notes: 'felt really tired today', workout_date: 'Feb 14 2018'},
+  {user_id: 8, distance: 5, time: 37, notes: 'It is cold today', workout_date: 'Feb 15 2018'},
+  {user_id: 8, distance: 10, time: 75, notes: 'Had a really tough run today', workout_date: 'Feb 16 2018'},
+  {user_id: 8, distance: 2, time: 17, notes: 'Recovery Day', workout_date: 'Feb 17 2018'},
+  {user_id: 8, distance: 15, time: 17, notes: 'Long Run Day', workout_date: 'Feb 18 2018'}
 ]
 
 # user.save!
