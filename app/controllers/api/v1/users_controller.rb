d068b80c13b6dec24707e1573e53a3c76dee7cf9
Sean Hanlon <shanlon800@gmail.com>
2018-02-19 17:28:36 -0500

    added strava workouts to current week if logged in

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,6 +11,33 @@ class Api::V1::UsersController < ApplicationController
       num_of_workouts_completed = User.find(current_user.id).workouts.length
 
 
+        if current_user.provider == 'strava'
+          strava_info =  HTTParty.get("https://www.strava.com/api/v3/athletes/#{current_user.uid}/stats?access_token=#{ENV['STRAVA_TOKEN']}")
+          year_to_date_strava = strava_info['ytd_run_totals']
+          all_workouts_before_strava = Workout.where(user_id: current_user.id)
+          strava_workouts = HTTParty.get("https://www.strava.com/api/v3/athletes/#{current_user.uid}/activities?access_token=#{ENV['STRAVA_TOKEN']}")
+          workout_id_list = []
+          all_workouts_before_strava.each do |workout|
+            workout_id_list << workout.id
+          end
+
+          strava_workouts_in_system = []
+          new_strava_workouts = []
+          strava_workouts.each do |workout|
+            if workout_id_list.include?(workout['id'])
+              strava_workouts_in_system << workout
+            else
+              new_strava_workouts << workout
+            end
+          end
+
+          new_strava_workouts.each do |workout|
+            Workout.create(id: workout['id'], user_id: current_user.id, distance: (workout['distance'].to_f * 0.000621).round(1), time: (workout['elapsed_time'].to_f / 60), notes: workout['name'], workout_date: workout['start_date_local'])
+          end
+        end
+
+
+
       @teams_with_goals = []
       @teams.each do |team|
         team_goal = {team: team, goal: team.goal}
@@ -43,6 +70,7 @@ class Api::V1::UsersController < ApplicationController
       @two_week_back_workouts = Workout.prior_week_workouts(2, current_user)
       @three_week_back_workouts = Workout.prior_week_workouts(3, current_user)
       @four_week_back_workouts = Workout.prior_week_workouts(4, current_user)
+
 
 
       total_miles_week = 0
@@ -107,9 +135,7 @@ class Api::V1::UsersController < ApplicationController
       week_dropdown = current_week_all_dates - user_current_week_workouts_dates
 
 
-      strava_info =  HTTParty.get("https://www.strava.com/api/v3/athletes/#{current_user.uid}/stats?access_token=#{ENV['STRAVA_TOKEN']}")
-      year_to_date_strava = strava_info['ytd_run_totals']
-      
+
       render json: {
         current_user: @current_user,
         workouts: @workouts,
