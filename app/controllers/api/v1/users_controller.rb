c4f6ed83bfe57e8098ab4242ee76a7a666a43a80
Sean Hanlon <shanlon800@gmail.com>
2018-02-19 17:40:11 -0500

    wrote a method for calculating strava workouts

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,24 +16,9 @@ class Api::V1::UsersController < ApplicationController
           year_to_date_strava = strava_info['ytd_run_totals']
           all_workouts_before_strava = Workout.where(user_id: current_user.id)
           strava_workouts = HTTParty.get("https://www.strava.com/api/v3/athletes/#{current_user.uid}/activities?access_token=#{ENV['STRAVA_TOKEN']}")
-          workout_id_list = []
-          all_workouts_before_strava.each do |workout|
-            workout_id_list << workout.id
-          end
 
-          strava_workouts_in_system = []
-          new_strava_workouts = []
-          strava_workouts.each do |workout|
-            if workout_id_list.include?(workout['id'])
-              strava_workouts_in_system << workout
-            else
-              new_strava_workouts << workout
-            end
-          end
+          Workout.add_strava_workouts(strava_workouts, all_workouts_before_strava, current_user)
 
-          new_strava_workouts.each do |workout|
-            Workout.create(id: workout['id'], user_id: current_user.id, distance: (workout['distance'].to_f * 0.000621).round(1), time: (workout['elapsed_time'].to_f / 60), notes: workout['name'], workout_date: workout['start_date_local'])
-          end
         end
 
 
